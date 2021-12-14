ad5ddcdb020e4e876cfb03d3ad4b39d4cf8f6279
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2021-12-14 17:38:27 +0100

    Changed paths of images and started fetch MoreInfo

--- src/script.js
+++ src/script.js
@@ -6,41 +6,99 @@ window.onload = function () {
 
     const apiKey = '1f10bd3195bf40e7b89a3c607aefcdef'
 
-    function callRegister() {
-
-        document.getElementById("form").addEventListener("submit", e => {
-
-            const firstNameValue = document.getElementById("fname").value
-            const lastNameValue = document.getElementById("lname").value
-            const emailValue = document.getElementById("email").value
-            const passwordValue = document.getElementById("password").value
 
 
-            let registerForm = {
-                firstname: firstNameValue,
-                lastname: lastNameValue,
-                email: emailValue,
-                password: passwordValue
-            }
+    // document.getElementById("form").addEventListener("submit", e => {
+    //     e.preventDefault();
 
+    //     const firstNameValue = document.getElementById("fname").value
+    //     const lastNameValue = document.getElementById("lname").value
+    //     const emailValue = document.getElementById("email").value
+    //     const passwordValue = document.getElementById("password").value
 
-            fetch('https://challenge-app-team-guillaume.herokuapp.com/challenges', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    firstname: firstNameValue,
-                    lastname: lastNameValue,
-                    email: emailValue,
-                    password: passwordValue
-                })
-            }).then(data => {
-                return data.json()
-            })
+    //     fetch('https://challenge-app-team-guillaume.herokuapp.com/challenges', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             firstname: firstNameValue,
+    //             lastname: lastNameValue,
+    //             email: emailValue,
+    //             password: passwordValue
+    //         })
+    //     }).then(data => {
+    //         return data.json()
+    //     })
 
-        })
+    // })
+
+    async function fetchMoreInfo() {
+        const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}&metacritic=75,100&dates=2021-11-01,2021-12-01`);
+        const movies = await response.json();
+        return movies;
     }
+
+    fetchMoreInfo().then(game => {
+
+
+        let gameResult = game.results
+
+        gameResult.forEach(game => {
+            console.log(game)
+            let gameName = game.name;
+            let gameImg = game.background_image;
+            let gameMetacritic = game.metacritic;
+            let gameRelease = game.released;
+
+            let container = document.getElementById("bigcontainergame");
+            let htmlString = "";
+
+            htmlString += `   <div class="bodycontainergame">
+            <div class="leftsidegameinfo">
+                <div class="gameheader">
+                    <div class="nameandscore">
+                        <h2 class="gamename">${gameName}</h2>
+                        <h2 class="metascore">${gameMetacritic}</h2>
+                    </div>
+                    <h3 class="gamestudio">${}</h3>
+                </div>
+                <h2 class="about">About</h2>
+                <p class="gameinfo">${}</p>
+                <h2 class="releasedate_h2">Release date</h2>
+                <h3 class="releasedate_h3">${}</h3>
+
+                <div class="release_platforms">
+                    <div class="releasedate_div">
+                        <h2 class="releasedate_h2">Platforms</h2>
+                        <h3 class="releasedate_h3">- ${}</h3>
+                        <h3 class="releasedate_h3">- ${}</h3>
+                    </div>
+
+                    <div class="platforms_div">
+                        <h2 class="releasedate_h2">Genre</h2>
+                        <h3 class="releasedate_h3">- ${}</h3>
+                        <h3 class="releasedate_h3">- ${}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="rightsidegameinfo">
+                <img src="${}" class="bigimggame" alt="Avatar">
+            </div>
+        </div>
+        <h2 class="pictures_h2">More pictures :</h2>
+        <div class="pictures_gameinfo">
+            <img src="${}" alt="Avatar">
+            <img src="${}" alt="Avatar">
+            <img src="${}" alt="Avatar">
+            <img src="${}" alt="Avatar">
+            <img src="${}" alt="Avatar">
+            <img src="${}" alt="Avatar">
+        </div>`
+            container.insertAdjacentHTML("beforeend", htmlString);
+        });
+    })
+
 
     async function fetchGotm() {
         const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}&metacritic=75,100&dates=2021-11-01,2021-12-01`);
@@ -49,12 +107,12 @@ window.onload = function () {
     }
 
     fetchGotm().then(game => {
-        console.log(game);
+
 
         let gameResult = game.results
 
         gameResult.forEach(game => {
-            console.log(game);
+
 
             let gameName = game.name;
             let gameImg = game.background_image;
@@ -84,12 +142,12 @@ window.onload = function () {
     }
 
     fetchPopular().then(game => {
-        console.log(game);
+
 
         let gameResult = game.results
 
         gameResult.forEach(game => {
-            console.log(game);
+
 
             let gameName = game.name;
             let gameImg = game.background_image;
@@ -121,12 +179,12 @@ window.onload = function () {
     }
 
     fetchAllGames().then(game => {
-        console.log(game);
+
 
         let gameResult = game.results
 
         gameResult.forEach(game => {
-            console.log(game);
+
 
             let gameName = game.name;
             let gameImg = game.background_image;
@@ -154,12 +212,12 @@ window.onload = function () {
     })
 
     fetchAllGames().then(game => {
-        console.log(game);
+
 
         let gameResult = game.results
 
         gameResult.forEach(game => {
-            console.log(game);
+
 
             let gameName = game.name;
             let gameImg = game.background_image;
